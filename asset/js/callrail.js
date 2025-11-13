bea8cc918ee5397911120d6146298ade3a28044a
jan <jeplam.08@gmail.com>
2025-11-13 18:01:39 +0800

    fix: changed callrail script

---
+++ asset/js/callrail.js
@@ -0,0 +1,6 @@
+(function() {
+    const script = document.createElement("script");
+    script.src = "//cdn.callrail.com/companies/270602115/e6781caa88cd9aa8464c/12/swap.js";
+    script.type = "text/javascript";
+    document.head.appendChild(script);
+})();
